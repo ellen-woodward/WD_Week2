8366daf36f1594cf4b3563797cd5ca97ee607501
Ellen Woodward <S00222321@atu.ie>
2023-01-31 15:21:33 +0000

    Updated title and added for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'week2';
+  title = 'Incredible To Do App';
+  thingsToDo = [
+    'Learn Javascript',
+    'Learn Angular',
+    'Learn Regex'
+  ];
 }
